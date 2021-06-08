a489194e0f906fbec45d3014545f1f1fcd5056ff
MaksimDannik <Scouts1@ukr.net>
2021-06-08 16:42:15 +0300

    how to work with moduls slugify

--- ap.js
+++ ap.js
@@ -1,40 +1,28 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
-
+const slugify = require('slugify');
+const replaseTemplate = require('./modules/replaseTemplate');
 
 
 // SERVER
-const replaseTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-}
 
 
-const tempOverview = fs.readFileSync(`N.js/template/template-overview.html`, 'utf-8');
-const tempCard = fs.readFileSync(`N.js/template/template-card.html`, 'utf-8');
-const tempProduct = fs.readFileSync(`N.js/template/template-product.html`, 'utf-8');
+
+const tempOverview = fs.readFileSync(`template/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`template/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`template/template-product.html`, 'utf-8');
 
 
-const data = fs.readFileSync(`N.js/dev-data/data.json`, 'utf-8');
+const data = fs.readFileSync(`dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true })) // slugify
 
 const server = http.createServer((req, resp) => {
 
     const { query, path } = url.parse(req.url, true);
-    // const path = req.url;
-    console.log(url.parse(req.url, true));
-    console.log(`QUERY  ${query.id}`);
-    // console.log(dataObj[query.id]);
 
     // Overview page
     if (path === '/' || path === '/overview') {
